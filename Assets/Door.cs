2b9c44447ba60e47970968717753c60229c545a7
Yiming Zhang <allenzym@gmail.com>
2020-03-04 21:22:07 -0800

    Door

---
+++ Assets/Door.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private GameObject keyPrefab;
+    [SerializeField] private Transform spawnPoint;
+    private GameObject myKey;
+    private Player playerScript;
+
+    private void Start()
+    {
+        spawnKey();
+    }
+
+    private void Update()
+    {
+        if (myKey == null)
+        {
+            spawnKey();
+        }
+    }
+
+    private void spawnKey()
+    {
+        myKey = Instantiate(keyPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerScript = other.GetComponent<Player>();
+            if (playerScript.hasKey)
+            {
+                Destroy(gameObject);
+                Destroy(myKey);
+                playerScript.keyInRange = false;
+                playerScript.hasKey = false;
+            }
+        }
+    }
+}
