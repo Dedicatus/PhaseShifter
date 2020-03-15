e181b5d3d658ea81cb090160764b93c6a77146de
Yiming Zhang <allenzym@gmail.com>
2020-03-14 22:44:24 -0700

    FreezeRotationTrigger

---
+++ Assets/FreezeRotationTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeRotationTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject stone;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(stone.transform.position.x, stone.transform.position.y + 3.5f, stone.transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            stone.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            stone.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+}
