f24d8e97904d37498066de50a1793ae81f5495fd
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2020-03-15 01:14:34 -0700

    update player jump judgment

--- Assets/FreezeRotationTrigger.cs
+++ Assets/FreezeRotationTrigger.cs
@@ -22,14 +22,21 @@ public class FreezeRotationTrigger : MonoBehaviour
         if (other.tag == "Player")
         {
             stone.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+            stone.GetComponent<Collider>().material.dynamicFriction = 99999999.9f;
+            stone.GetComponent<Collider>().material.staticFriction = 99999999.9f;
+            //other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
         }
     }
 
     private void OnTriggerExit(Collider other)
-    {
+    {   
         if (other.tag == "Player")
         {
             stone.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            stone.GetComponent<Collider>().material.dynamicFriction = 0.9f;
+            stone.GetComponent<Collider>().material.staticFriction = 0.9f;
+            //other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            //other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
         }
     }
 }
