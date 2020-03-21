aebdacdeada17b8df19458cedab4630e1f6a14c7
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2020-03-20 19:46:27 -0700

    update waterfall sound

---
+++ Assets/WaterfallAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterfallAudio : MonoBehaviour
+{
+    [FMODUnity.EventRef] public string WaterfallEvent = "event:/WaterFall";
+    FMOD.Studio.EventInstance Waterfall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Waterfall = FMODUnity.RuntimeManager.CreateInstance(WaterfallEvent);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(Waterfall, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+    private void OnEnable()
+    {
+        Waterfall.start();
+    }
+
+    private void OnDisable()
+    {
+        Waterfall.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
