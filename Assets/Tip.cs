6ad63fc61082060e91a7ff3a617ce865947c1913
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2020-03-24 20:57:53 -0700

    update ui

---
+++ Assets/Tip.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private UIController uc;
+    [SerializeField] private string tip;
+
+    void Start()
+    {
+        uc = GameObject.FindWithTag("UIController").GetComponent<UIController>();
+    }
+
+// Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            uc.setTip(tip);
+            uc.showTip();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            uc.closeTip();
+        }
+    }
+}
